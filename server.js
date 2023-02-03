67391fa1a36a76b340a3486eb86aef11a403f854
Sebastian Fripp <sebastian.fripp@ibm.com>
2023-02-03 12:40:23 -0300

    Tekton changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = "0.0.0.0"
 // App
 const app = express()
 app.get("/", (req, res) => {
-    const msg = process.env.persona ? process.env.persona : 'testeo'
+    const msg = process.env.persona ? process.env.persona : 'from Tekton Toolchain'
     res.send(`Hello ${msg}!`)
 })
 
